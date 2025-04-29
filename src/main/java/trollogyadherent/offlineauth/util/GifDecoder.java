213e560a82564d9f0db5a7005633d7814d8e33f3
kotmatross28729 <110309314+kotmatross28729@users.noreply.github.com>
2025-04-29 18:34:00 +0300

    CHANGELOG

--- src/main/java/trollogyadherent/offlineauth/util/GifDecoder.java
+++ src/main/java/trollogyadherent/offlineauth/util/GifDecoder.java
@@ -13,6 +13,7 @@ import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 
 /**
